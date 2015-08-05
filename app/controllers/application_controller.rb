4a21de7e48e26a43ecc5c87e2b7032fd4b2db3b1
elainapolson <elainapolson@gmail.com>
2015-08-05 10:28:38 -0400

    add authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   	protect_from_forgery with: :exception
-  	helper_method :current_user
+  	helper_method :current_user, :login_required, :logged_in?
  
-  	private
+    before_action :login_required
+  	
+    private
  
   	def current_user
     	@current_user ||= User.find(session[:user_id]) if session[:user_id]
   	end
+
+    def login_required
+      if !logged_in?
+        redirect_to root_path, :notice => "You need to login"
+      end
+    end
+
+    def logged_in?
+      !!current_user
+    end 
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,18 @@
 # sessions_controller.rb
 class SessionsController < ApplicationController   
 	
+  skip_before_action :login_required, only: :create
+
 	def create     
 		auth = request.env["omniauth.auth"]     
 		user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)     
 		session[:user_id] = user.id     
 		redirect_to root_url, :notice => "Signed in!"
-  	end
+  end
  
-  	def destroy
-    	session[:user_id] = nil
-    	redirect_to root_url, :notice => "Signed out!"
-  	end
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Signed out!"
+  end
  
 end
