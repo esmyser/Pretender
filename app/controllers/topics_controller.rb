d5cdbe4a73bfb12e958f97007ac649248da305d2
gadbaw <bengadbaw@gmail.com>
2015-08-05 13:25:50 -0400

    minor bug fixes

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -32,7 +32,7 @@ class TopicsController < ApplicationController
   def destroy
     @topic = Topic.find(params['id'])
     @topic.destroy
-    redirect_to user_path(User.find(params['user_id']))
+    redirect_to(:back)
   end
 
   private
