3bf272d9f17f053f7467d4b520eacc09055baba1
elainapolson <elainapolson@gmail.com>
2015-08-10 15:41:16 -0400

    emails for topic working with whenever gem

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,16 @@
 set :environment, "development"
 
-every 1.day, :at => '6:00 am' do
-  rake "emails:send_report_emails_every_day"
+every 1.day, :at => '3:40 pm' do
+  rake "emails:send_pretendee_report_emails_every_day"
+  rake "emails:send_topic_report_emails_every_day"
 end
 
-every 3.days, :at => '6:00 am' do
-  rake "emails:send_report_emails_every_three_days"
+every 3.days, :at => '3:40 pm' do
+  rake "emails:send_pretendee_report_emails_every_three_days"
+  rake "emails:send_topic_report_emails_every_three_days"
 end
 
-every 7.days, :at => '6:00 am' do
-  rake "emails:send_report_emails_every_week"
+every 7.days, :at => '3:40 pm' do
+  rake "emails:send_pretendee_report_emails_every_week"
+  rake "emails:send_topic_report_emails_every_week"
 end
