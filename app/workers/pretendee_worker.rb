d796304176b2518a763af1d6b055118ece01fce3
gadbaw <bengadbaw@gmail.com>
2015-08-11 19:07:33 -0400

    fixes error

--- app/workers/pretendee_worker.rb
+++ app/workers/pretendee_worker.rb
@@ -7,10 +7,12 @@ class PretendeeWorker
 		pretendee.name = twitter_wrapper.get_name
 		pretendee.tweets = twitter_wrapper.recent_tweets
     pretendee.word_histogram = twitter_wrapper.word_count_histogram
-    greatest_weight = pretendee.word_histogram.first[:weight] + 20
+    greatest_weight = pretendee.word_histogram.first["weight"].to_i + 20
     pretendee.word_histogram.unshift({text: pretendee.name, weight: greatest_weight})
     instagram_wrapper = InstagramWrapper.new
     insta_id = instagram_wrapper.get_id(twitter_wrapper.photo_id)
     pretendee.instagram_photos = instagram_wrapper.recent_instgrams(insta_id)
+    pretendee.save
 	end
 end
+
