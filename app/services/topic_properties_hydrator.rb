98af65911a29246144460802bdb2527b44891e75
esmyser <esmyser@gmail.com>
2015-08-11 16:20:25 -0400

    adds sidekiq for topics

--- app/services/topic_properties_hydrator.rb
+++ app/services/topic_properties_hydrator.rb
@@ -4,10 +4,7 @@ class TopicPropertiesHydrator
   attr_accessor :ny_times_articles, :wiki_text, :tweets
 
   def initialize(topic)
-    topic.ny_times_articles = NyTimesWrapper.new.articles(topic)
-    topic.wiki_text = WikiWrapper.new.wiki_text(topic)
-    topic.tweets = TwitterWrapper.new(Pretendee.new).popular_tweets(topic)
-    topic.save
+    TopicWorker.perform_async(topic)
   end
 
 end
