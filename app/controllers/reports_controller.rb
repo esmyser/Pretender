afdb213853b6a36dbc39365f7394730368f8a33d
elainapolson <elainapolson@gmail.com>
2015-08-12 13:36:39 -0400

    edit user model associations to have reports through topics

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,17 +19,17 @@ class ReportsController < ApplicationController
   end
 
   def update
-    binding.pry
     @report = Report.find(params['id'])
     @report.update(report_params)
     @user = current_user
     if @report.save && report_params['pretendee_id']
+      @pretendee = Pretendee.find(report_params['pretendee_id'])
       respond_to do |format|
         format.html {redirect_to user_pretendee_path(@user, @pretendee)}
         format.js
       end
     elsif @report.save && report_params['topic_id']
-      binding.pry
+      @topic = Topic.find(report_params['topic_id'])
       respond_to do |format|
         format.html {redirect_to user_topic_path(@user, @topic)}
         format.js
