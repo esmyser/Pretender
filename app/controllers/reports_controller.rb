19dfa330000c76940b81b04fd49fef4cf05a56b1
elainapolson <elainapolson@gmail.com>
2015-08-12 18:27:49 -0400

    updated ajax and email forms

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,17 +3,21 @@ class ReportsController < ApplicationController
   def create
     @report = Report.new(report_params)
     @user = current_user
-    if @report.save && report_params['pretendee_id']
+    if report_params['pretendee_id']
       @pretendee = Pretendee.find(report_params['pretendee_id'])
-      respond_to do |format|
-        format.html {redirect_to user_pretendee_path(@user, @pretendee)}
-        format.js
+      if @report.save
+        respond_to do |format|
+          format.html {redirect_to user_pretendee_path(@user, @pretendee)}
+          format.js
+        end
       end
-    elsif @report.save && report_params['topic_id']
+    elsif report_params['topic_id']
       @topic = Topic.find(report_params['topic_id'])
-      respond_to do |format|
-        format.html {redirect_to user_topic_path(@user, @topic)}
-        format.js
+      if @report.save 
+        respond_to do |format|
+          format.html {redirect_to user_topic_path(@user, @topic)}
+          format.js
+        end
       end
     end
   end
@@ -22,17 +26,21 @@ class ReportsController < ApplicationController
     @report = Report.find(params['id'])
     @report.update(report_params)
     @user = current_user
-    if @report.save && report_params['pretendee_id']
+    if report_params['pretendee_id']
       @pretendee = Pretendee.find(report_params['pretendee_id'])
-      respond_to do |format|
-        format.html {redirect_to user_pretendee_path(@user, @pretendee)}
-        format.js
+      if @report.save 
+        respond_to do |format|
+          format.html {redirect_to user_pretendee_path(@user, @pretendee)}
+          format.js
+        end
       end
-    elsif @report.save && report_params['topic_id']
+    elsif report_params['topic_id']
       @topic = Topic.find(report_params['topic_id'])
-      respond_to do |format|
-        format.html {redirect_to user_topic_path(@user, @topic)}
-        format.js
+      if @report.save 
+        respond_to do |format|
+          format.html {redirect_to user_topic_path(@user, @topic)}
+          format.js
+        end
       end
     end
   end
