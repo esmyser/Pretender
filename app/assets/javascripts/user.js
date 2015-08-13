467853e8c62152a42a4330830c35b0531c864566
elainapolson <elainapolson@gmail.com>
2015-08-13 18:34:59 -0400

    button is delayed but not fading yet

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -29,36 +29,35 @@ $(function(){
     });
   });
 
+  $("#new_pretendee").submit(function(event){
+    event.preventDefault();
+    event.stopPropagation();
 
-$("#new_pretendee").submit(function(event){
-  event.preventDefault();
-  event.stopPropagation();
+    var pusher = new Pusher('01e0b165f94105952d85');
+    // need to get figaro working
+    var name = $("#the_user").text();
+    var channel = pusher.subscribe(name);
+    // need to set separate channels for each topic creation
+    
+    channel.bind('finished', function(data) {
+      var url = window.location.href
+      var html = "<div class='image-wrapper col-sm-3 nopadding'><div class='square-div'><img src='" + data.pretendee_image + "' class='img-responsive square-img'></div><div class='image-overlay'><p><a href='" + url + "/pretendees/" + data.pretendee_id + "'>" + data.pretendee_name + "</a><br><br><a rel='nofollow' data-method='delete' href='" + url + "/pretendees/" + data.pretendee_id + "'>Delete</a></p></div></div>";
+      $("#pretendees").children().last().remove();
+      $("#pretendees").append(html);
+      pusher.unsubscribe($("#the_user").text());
+    });
 
-  var pusher = new Pusher('01e0b165f94105952d85');
-  // need to get figaro working
-  var name = $("#the_user").text();
-  var channel = pusher.subscribe(name);
-  // need to set separate channels for each topic creation
-  
-  channel.bind('finished', function(data) {
-    var url = window.location.href
-    var html = "<div class='image-wrapper col-sm-3 nopadding'><div class='square-div'><img src='" + data.pretendee_image + "' class='img-responsive square-img'></div><div class='image-overlay'><p><a href='" + url + "/pretendees/" + data.pretendee_id + "'>" + data.pretendee_name + "</a><br><br><a rel='nofollow' data-method='delete' href='" + url + "/pretendees/" + data.pretendee_id + "'>Delete</a></p></div></div>";
-    $("#pretendees").children().last().remove();
-    $("#pretendees").append(html);
-    pusher.unsubscribe($("#the_user").text());
+    var url = $(this).attr('action'),
+        method = $(this).attr('method'),
+        data = $(this).serializeArray();
+
+    $.ajax({
+      method: method,
+      url: url,
+      data: data,
+      dataType: 'script'
+    });
   });
-
-  var url = $(this).attr('action'),
-      method = $(this).attr('method'),
-      data = $(this).serializeArray();
-
-  $.ajax({
-    method: method,
-    url: url,
-    data: data,
-    dataType: 'script'
-  });
-});
 
 
 });
