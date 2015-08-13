99d356c1e95a54b2ef9c1fbc118a4ed046147712
elainapolson <elainapolson@gmail.com>
2015-08-13 14:20:18 -0400

    gets url from browser to set pretendee and topic links

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -8,9 +8,10 @@ $(function(){
     var name = $("#the_user").text();
     var channel = pusher.subscribe(name);
     // need to set separate channels for each topic creation
-    
+
     channel.bind('finished', function(data) {
-      var html = "<div class='image-wrapper col-sm-3 nopadding'><div class='square-div'><img src='" + data['topic']['image'] + "' class='img-responsive square-img'></div><div class='image-overlay'><p><a href='http://localhost:3000/users/" + data['topic']['user_id'] + "/topics/" + data['topic']['id'] + "'>" + data['topic']['name'] + "</a><br><br><a rel='nofollow' data-method='delete' href='http://localhost:3000/users/" + data['topic']['user_id'] + "/topics/" + data['topic']['id'] + "'>Delete</a></p></div></div>"
+      var url = window.location.href 
+      var html = "<div class='image-wrapper col-sm-3 nopadding'><div class='square-div'><img src='" + data['topic']['image'] + "' class='img-responsive square-img'></div><div class='image-overlay'><p><a href='" + url + "/topics/" + data['topic']['id'] + "'>" + data['topic']['name'] + "</a><br><br><a rel='nofollow' data-method='delete' href='" + url + "/topics/" + data['topic']['id'] + "'>Delete</a></p></div></div>"
       $("#topics").children().last().remove();
       $("#topics").append(html);
       pusher.unsubscribe($("#the_user").text());
@@ -40,7 +41,8 @@ $("#new_pretendee").submit(function(event){
   // need to set separate channels for each topic creation
   
   channel.bind('finished', function(data) {
-    var html = "<div class='image-wrapper col-sm-3 nopadding'><div class='square-div'><img src='" + data.pretendee_image + "' class='img-responsive square-img'></div><div class='image-overlay'><p><a href='http://localhost:3000/users/" + data.user_id + "/pretendees/" + data.pretendee_id + "'>" + data.pretendee_name + "</a><br><br><a rel='nofollow' data-method='delete' href='http://localhost:3000/users/" + data.user_id + "/pretendees/" + data.pretendee_id + "'>Delete</a></p></div></div>";
+    var url = window.location.href
+    var html = "<div class='image-wrapper col-sm-3 nopadding'><div class='square-div'><img src='" + data.pretendee_image + "' class='img-responsive square-img'></div><div class='image-overlay'><p><a href='" + url + "/pretendees/" + data.pretendee_id + "'>" + data.pretendee_name + "</a><br><br><a rel='nofollow' data-method='delete' href='" + url + "/pretendees/" + data.pretendee_id + "'>Delete</a></p></div></div>";
     $("#pretendees").children().last().remove();
     $("#pretendees").append(html);
     pusher.unsubscribe($("#the_user").text());
