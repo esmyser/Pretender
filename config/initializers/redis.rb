cd162c5f1b273107cd501110f3ab0345a68b9ad7
esmyser <esmyser@gmail.com>
2015-08-13 17:36:54 -0400

    redis more redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
 uri = URI.parse("redis://redistogo:03d5b7f0973a86269bc16207879404a0@tarpon.redistogo.com:10927/")
-REDIS = Redis.new(:url => uri, port: uri.port, password: uri.password)
+REDIS = Redis.new(:url => uri)
