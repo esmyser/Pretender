544b39e85b95c1108e3efda4c92c7a8851cfaeb3
esmyser <esmyser@gmail.com>
2015-08-13 16:46:10 -0400

    sidekiq redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,3 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:url => uri)
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:3000")
+REDIS = Redis.new(:url => uri, port: uri.port, password: uri.password)
+Sidekiq.redis = REDIS
